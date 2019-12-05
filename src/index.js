a2baea15bc7c4e724752411bf8fd373c0ae4b90a
Georgia Connell-Bass <georgiaconnellwork@gmail.com>
2019-12-05 17:06:30 +0000

    Connected to accouts API and returned acc data, outputted account holder names onto page

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import useGetAccounts from "./GetAccounts";
+
+function App() {
+    const accounts = useGetAccounts()
+    const accountElements = accounts.map((account) =>
+        <p>{account.accountHolder}</p>
+    );
+    return (
+        <div className="App">
+            { accountElements }
+        </div>
+    );
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
